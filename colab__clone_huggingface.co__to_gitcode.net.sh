8d342da6e67c9569c02140e7a0aa60bdfa7ce919
prgrmz07 <prgrmz07@163.com>
2024-05-27 08:28:37 +0800

    改为 丢弃work-tree 只留git-dir、打包、拆小、小传到google网盘

--- colab__clone_huggingface.co__to_gitcode.net.sh
+++ colab__clone_huggingface.co__to_gitcode.net.sh
@@ -1,80 +1,33 @@
 #!/usr/bin/python3
 
-#【描述】   利用google的colab 克隆 25GB的华为开源大模型chatGLM预训练模型git仓库huggingface.co/THUDM/chatglm-6b 、打包仓库、拆小、小传到gitcode.net
-#              理论正确、能正常运行、但是git大文件很消耗时间、几乎可以放弃了，还不如将git替换为邮箱附件
+#【描述】   利用google的colab 克隆 25GB的华为开源大模型chatGLM预训练模型git仓库huggingface.co/THUDM/chatglm-6b 、丢弃work-tree 只留git-dir、打包、拆小、小传到google网盘
 #【依赖】   
 #【术语】  
 #【备注】   
 
 
+
+
+
 #1. 克隆 git仓库 huggingface.co/THUDM/chatglm-6b
-########
 git lfs install
 git clone https://huggingface.co/THUDM/chatglm-6b
-apt install -y tree 
-tree 
-ls -lh chatglm-6b
-mkdir  /app3
-mv chatglm-6b /app3/chatglm-6b-pretain 
 
-du -h /app3/chatglm-6b-pretain  #25G	/app3/chatglm-6b-pretain
+#2. colab页面上点击挂载google-drive（google网盘）
+#  google网盘挂载后目录为 /content/drive/MyDrive
 
-find /app3/chatglm-6b-pretain -type f -not -path "*\.git/*" -print0| xargs -0 -I@ sha256sum @ | tee  /app3/chatglm-6b-pretain.sha256.txt
+#3. 删除git仓库的work-tree,只保留git-dir。 因为google-drive只有15GB空间
+rm -fr chatglm-6b/*
+tar -zcvf  chatglm-6b.git.tar.gz chatglm-6b/.git/
 
-#2. 打包 git仓库
-#######
-tar -cvf /app3/chatglm-6b-pretain.tar /app3/chatglm-6b-pretain.sha256.txt  /app3/chatglm-6b-pretain
+#4. 包摘要上传到 google网盘
+md5sum chatglm-6b.git.tar.gz | tee /content/drive/MyDrive/chatglm-6b-data/chatglm-6b.git.tar.gz.md5sum.txt
 
-#3. 拆分为小文件 
-#########
-#后缀为 001,002,...,640
-split -b 40m  --numeric-suffixes=1  -d  --suffix-length=3 /app3/chatglm-6b-pretain.tar /app3/chatglm-6b-pretain.tar-25GB-40MB-
+#5. 拆分为小文件 并上传google网盘
+split -b 1024m  --numeric-suffixes=1  -d  --suffix-length=3 /content/chatglm-6b.git.tar.gz /content/drive/MyDrive/chatglm-6b-data/chatglm-6b.git.tar.gz-1GB-
 
-#4. 在给定gitcode.net组织下创建N个仓库,仓库名是1...N
-########
-PYTHONPATH=/app/bash-simplify/  python3 -c "import gitcode_net_api_demo as M; M.main__createNRepo_inGrp('org-chatglm-6b_dev2',640)"
+#6. 去google网盘下载 chatglm-6b-data/chatglm-6b.git.tar.gz-1GB-*
+#  浏览器点击下载后，当下载已开始后，禁用switchOmega 这样可以节省很大代理流量
 
-#5.0 准备
-#######
-gitcode_token="szgTdCTbpJ_ikGzZV5AR"
-gitcode_usr="prgrmz07"
-
-readarray -t fArr < <(ls /app3/chatglm-6b-pretain.tar-25GB-40MB-* )
-fCnt="${#fArr[@]}"
-
-repoHm=$(pwd)/gitcode_home
-mkdir -p $repoHm
-
-#5. 循环克隆N个gitcode.net仓库
-for (( k=1; k<=fCnt; k++ )); do
-  repokD=${repoHm}/${k}
-  repok_cln_cmd="git clone https://$gitcode_usr:$gitcode_token@gitcode.net/myz/org-chatglm-6b_dev2/${k}.git ${repokD}"
-  [[ -f ${repokD}/.git/config ]] || $repok_cln_cmd
-done
-
-#6. 循环, 小文件k移动到gitcode.net仓库k  并git添加、git提交
-###########
-
-for (( k=1; k<=fCnt; k++ )); do
-  echo -n "${k},"
-  repokD=${repoHm}/${k}
-  gitArg="--work-tree ${repokD} --git-dir ${repokD}/.git"
-  _k=$(printf "%03d\n" "$k")
-  fk="/app3/chatglm-6b-pretain.tar-25GB-40MB-${_k}"
-  # ls $fk  || continue
-  # { ls $fk  && mv $fk $repokD/ ;} || true
-  git $gitArg status
-  git $gitArg add '*' && git $gitArg commit -m "add chatglm-6b-pretain.tar-25GB-40MB-${_k}"
-done
-
-
-#7. 循环, gitcode.net仓库k  git推送
-###########
-
-for (( k=1; k<=fCnt; k++ )); do
-  echo -n "${k},"
-  repokD=${repoHm}/${k}
-  gitArg="--work-tree ${repokD} --git-dir ${repokD}/.git"
-  git $gitArg push
-done
-
+#7. 合并小文件为包
+cat /app3/chatglm-6b-data/chatglm-6b.git.tar.gz-1GB-??? > /app3/chatglm-6b.git.tar.gz
